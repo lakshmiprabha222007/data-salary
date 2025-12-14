5a7942c1710111fd9657e5da445ad0425482141c
lakshmiprabha222007 <lakshmiprabha222007@gmail.com>
2025-12-14 13:38:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ with open("trained_salary_LR_model.pkl", "rb") as f:
 st.title("Salary Prediction App")
 
 # Detect number of features automatically
-features = model.features_in_
+1_features = model.1_features_in_
 
 st.write(f"Model expects {n_features} input feature(s)")
 
